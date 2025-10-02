a53e0c987f52eb78ae0ce17cb18782c3fc4093ef
Kelly Halladay <khalladay@Halladay-Macbook.local>
2025-10-02 10:22:48 -0400

    Initial Commit

---
+++ src/routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Sample route
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello from the API route!'});
+});
+
+module.exports = router;
